0c8827274e1743e1d6996cc719d4a513bb4f9bf4
Julianibero <jvegajoy@estudiante.ibero.edu.co>
2026-05-02 20:49:20 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const proyectoRoutes = require('./src/routes/proyecto.routes');
 
 const app = express();
 
-aapp.use(cors({
+app.use(cors({
   origin: [
     'http://localhost:3001',
     'http://localhost:3000',
